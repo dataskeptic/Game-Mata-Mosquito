c5ea18b8ece888208ca9ccb2afbd993428e160d3
mathenrifs <mateus_henrique2011@outlook.com>
2020-05-14 20:43:25 -0300

    add

--- jogo.js
+++ jogo.js
@@ -2,18 +2,34 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 
 function ajustaTamanhoPalcoJogo() {
 
 	altura = window.innerHeight
 	largura = window.innerWidth
-	console.log(largura, altura) 
+	
 }
 
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
+
+	// Remover o mosquito anterior (caso exista) ---------------------------------------------------------
+	if (document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+
+		if(vidas > 3) {
+			
+			window.location.href = 'fim_de_jogo.html'	
+		} else {
+		
+		document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+		vidas++
+		}	
+	}
+
 
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -21,9 +37,8 @@ function posicaoRandomica() {
 	posicaoX = posicaoX < 0 ? 0 : posicaoX
 	posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-	console.log(posicaoX, posicaoY)
-
-	// Criar o elemtento html
+	
+	// Criar o elemtento html ---------------------------------------------------------
 
 	var mosquito = document.createElement('img') 
 	mosquito.src = 'imagens/mosquito.png' 
@@ -31,13 +46,16 @@ function posicaoRandomica() {
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+	mosquito.id = 'mosquito'
+	mosquito.onclick = function() {
+		this.remove()
+	}
 
 	document.body.appendChild(mosquito)
-	tamanhoAleatorio()
+	
 }
 function tamanhoAleatorio() {
 	var classe = Math.floor(Math.random() * 3)
-	console.log(classe)
 
 	switch(classe) {
 		case 0:
